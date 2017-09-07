2525cd5e41bad205b4cd3a81d0a6119cbdc250c4
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-07 15:33:57 -0400

    Update modification

--- workouts/log.js
+++ workouts/log.js
@@ -15,19 +15,6 @@ $(function(){
 				$("#update-definition").append(opts);
 
 			},
-
-			updateDefinitions: function(){
-				var defs = WorkoutLog.definition.userDefinitions;
-				console.log("test update-definition")
-				var len = defs.length;
-				var opts = "";
-					for (var i = 0; i < len; i++){
-						opts += "<option value='" + defs[i].id + "'>" + defs[i].description + "</option>";
-					}
-				$("#update-definition").children().remove();
-				$("#update-definition").append(opts);
-			},
-
 			setHistory: function() {
 				var history = WorkoutLog.log.workouts;
 				var len = history.length;
@@ -67,9 +54,6 @@ $(function(){
 					$("a[href='#history']").tab("show");
 				});
 			},
-
-
-
 			getWorkout: function(){
 				var thisLog = { id: $(this).attr("id")};
 				console.log("thisLog");
@@ -87,6 +71,35 @@ $(function(){
 					$("#update-description").val(data.description);
 					$("#update-id").val(data.id);
 					WorkoutLog.log.updateDefinitions();
+				});
+			},
+			updateWorkout: function() {
+				$("#update").text("Update");
+				var updateLog = { 
+					id: $('#update-id').val(),
+					desc: $("#update-description").val(),
+						result: $("#update-result").val(),
+						def: $("#update-definition option:selected").text()
+				};
+				for(var i = 0; i < WorkoutLog.log.workouts.length; i++){
+					if(WorkoutLog.log.workouts[i].id == updateLog.id){
+						WorkoutLog.log.workouts.splice(i, 1);
+					}
+				}
+				WorkoutLog.log.workouts.push(updateLog);
+				var updateLogData = { log: updateLog };
+				var updater = $.ajax({
+						type: "PUT",
+						url: WorkoutLog.API_BASE + "log",
+						data: JSON.stringify(updateLogData),
+						contentType: "application/json"
+				});
+
+				updater.done(function(data) {
+					console.log(data);
+					$("#update-description").val("");
+					$("#update-result").val("");
+					$('a[href="#history"]').tab("show");
 				});
 			},
 			delete: function(){
