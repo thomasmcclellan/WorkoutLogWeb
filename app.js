8edc00141c454098049b22389ffb91d9c9017dd9
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-07 13:31:46 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+$(function(){
+	var WorkoutLog = (function($, undefined){
+		var API_BASE = "https://equally-yoked-api.herokuapp.com/";
+		var userDefinitions = []
+
+		var setAuthHeader = function(sessionToken){
+			window.localStorage.setItem("sessionToken", sessionToken);
+			//Set the authorization header
+			//This can be done on individual calls
+			//Here, we showcase ajaxSetup as a global tool
+			$.ajaxSetup({
+				"headers": {
+					"Authorization": sessionToken
+				}
+			});
+		};
+
+		//public
+		return {
+			API_BASE: API_BASE,
+			setAuthHeader: setAuthHeader
+		};
+
+	})(jQuery);
+
+	//Ensure .disabled aren't clickable
+	$(".nav-tabs a[data-toggle=tab]").on("click", function(e){
+		if ($(this).hasClass("disabled") && !token){
+			e.preventDefault();
+			return false;
+		}
+	});
+
+	//bind enter key
+	$(document).on("keypress", function(e){
+		if (e.which === 13){
+			if ($("#signup-modal").is(":visible")){
+				$("#signup").trigger("click");
+			} 
+			if ($("#login-modal").is(":visible")){
+				$("#login").trigger("click");
+			}
+		}
+	});
+
+	//bootstrap tab => binding to a bootstrap event
+	$('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
+		var target = $(e.target).attr("href"); //activated tab
+		if (target === "#log"){
+			WorkoutLog.log.setDefinitions();
+		}
+		if (target === "#update-log"){
+			WorkoutLog.log.setDefinitions();
+		}
+		if (target === "#history"){
+			WorkoutLog.log.setHistory();
+		}
+	}); 
+
+	//setHeader if we
+	var token = window.localStorage.getItem("sessionToken");
+	if (token){
+		WorkoutLog.setAuthHeader(token);
+	}
+
+	//expose this to the other workoutlog modules
+	window.WorkoutLog = WorkoutLog;
+});
+
+
+
+
+// $(document).ready(function(){
+// 	$('#testAPI').on('click', function(){
+// 		console.log('working!!!');
+// 	});
+
+// 	var test = $.ajax({
+// 		type: 'GET',
+// 		url: 'http://localhost:3000/api/test'
+// 	})
+// 	.done(function(data){
+// 		console.log(data);
+// 	})
+// 	.fail(function(){
+// 		console.log('Oh no!');
+// 	});
+// });
---
+++ user/auth.js
@@ -0,0 +1,105 @@
+$(function(){
+	$.extend(WorkoutLog, {
+		//signup method
+		signup: function(){
+			//username and password variables
+			var username = $('#su_username').val();
+			var password = $('#su_password').val();
+			//user object
+			var user = {
+				user: {
+					username: username,
+					password: password
+				}
+			};
+
+			//signup post
+			var signup = $.ajax({
+				type: "POST", 
+				url: WorkoutLog.API_BASE + "user",
+				data: JSON.stringify(user),
+				contentType: "application/json"
+			});
+			//signup done/fail
+			signup
+				.done(function(data){
+					if (data.sessionToken){
+						WorkoutLog.setAuthHeader(data.sessionToken);
+						WorkoutLog.definition.fetchAll();
+						WorkoutLog.log.fetchAll();
+
+						console.log("You made it!");
+						console.log(data.sessionToken);
+					}
+
+					$('#signup-modal').modal('hide');
+					$('.disabled').removeClass('disabled');
+					$('#loginout').text('Logout');
+					$("#su_username").val("");
+					$("#su_password").val("");
+					//routing
+					$("a[href='#define']").tab('show');
+				})
+				.fail(function(){
+					$('#su_error').text("There was an issue with sign up").show();
+				});
+		},
+		//login method
+		login: function(){
+			//login variables
+			var username = $("#li_username").val();
+			var password = $("#li_password").val();
+			var user = {
+				user: {
+					username: username,
+					password: password
+				}
+			};
+			//login POST
+			var login = $.ajax({
+				type: "POST", 
+				url: WorkoutLog.API_BASE + "login",
+				data: JSON.stringify(user),
+				contentType: "application/json"
+			});
+			//login done/fail
+			login
+				.done(function(data){
+					if (data.sessionToken){
+						WorkoutLog.setAuthHeader(data.sessionToken);
+						WorkoutLog.definition.fetchAll();
+						WorkoutLog.log.fetchAll();
+						
+						console.log(data.sessionToken);
+					}
+					$("#login-modal").modal("hide");
+					$(".disabled").removeClass("disabled");
+					$("#loginout").text("Logout");
+					
+					$("#li_username").val("");
+					$("#li_password").val("");
+					$("a[href='#define']").tab("show");
+				})
+				.fail(function(){
+					$("#li_error").text("There was an issue with sign up").show();
+				});
+		},
+		//loginout method
+		loginout: function(){
+			if (window.localStorage.getItem("sessionToken")){
+				window.localStorage.removeItem("sessionToken");
+				$("#loginout").text("Login");
+			}
+
+			//TODO: on logout, make sure stuff is disabled
+		}
+	});
+	//bind events
+	$("#login").on("click", WorkoutLog.login);
+	$('#signup').on('click', WorkoutLog.signup);
+	$("#loginout").on("click", WorkoutLog.loginout);
+
+	if (window.localStorage.getItem("sessionToken")){
+		$("#loginout").text("Login");
+	}
+});
